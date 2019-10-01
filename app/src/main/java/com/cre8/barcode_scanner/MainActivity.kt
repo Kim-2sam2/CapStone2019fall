7524892ca4ca298d5c13b9ae57283644b81a1582
Kim-2sam2 <Kim-2sam2@github.com>
2019-10-01 15:10:11 +0900

    스캔기능

--- app/src/main/java/com/cre8/barcode_scanner/MainActivity.kt
+++ app/src/main/java/com/cre8/barcode_scanner/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.cre8.barcode_scanner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var intent = Intent(this, Scanner::class.java)
+        startActivity(intent)
     }
 }
