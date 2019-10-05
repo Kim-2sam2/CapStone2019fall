0e17a46a1cf569de2a3c7525d4f977819f719a69
sangwoooo <dcq306@naver.com>
2019-10-05 22:31:22 +0900

    change_top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,9 @@
 <div class="footer" style="margin-top: 100px;">
         <div class="logo">
-            <a href="https://www.mcst.go.kr/kor/main.jsp" style="padding-right: 50px;"><img src="/images/korea_culture.png"  width="250" height="50"></a>
-            <a href="http://urimal.hnu.kr/korea/html/new_open01.html" style="padding-right: 50px;"><img src="/images/logo.png" width="250" height="50"></a>
+            <a href="http://www.hannam.ac.kr/main/" style="padding-right: 50px;"><img src="/images/hannam.png" width="200" height="50"></a>
+            <a href="https://hanul.hannam.ac.kr" style="padding-right: 50px;"><img src="/images/hannam_lib.png"  width="250" height="50"></a>
+
             {{-- <a href="http://www.xn--3e0bp5xb8hr6ao19b.com/default/main/main.php"><img src="/images/kruisler.png"></a> --}}
-            <b>문의사항  -  한남대학교 국어문화원 대표전화 : <a href="tel:042-629-7474">042-629-7474</a></b>
+            <b>문의사항  -  한남대학교 중앙도서관 대표전화 : <a href="tel:042-629-7689">042-629-7474(대출/반납)</a></b>
         </div>
 </div>
