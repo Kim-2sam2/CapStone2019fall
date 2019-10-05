3fca9524496f17e5845d0ff0f54a71a8b05e0fd7
sangwoooo <dcq306@naver.com>
2019-10-05 20:54:20 +0900

    change

--- resources/views/guideline.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-<div class="guideline">
-        <embed src="/file/guideline.pdf" type="application/pdf" width="1250" height="700" />
-</div>
-@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>국어문화원</title>
+        <title>Smart library</title>
         <!-- Styles -->
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <script src="./js/main.js"></script>
